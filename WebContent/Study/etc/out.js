362f81fa10047ad3abefb710d4c5833a47a6b86f
dhktjdyd12 <55132488+dhktjdyd12@users.noreply.github.com>
2019-10-23 17:38:55 +0900

    2019.10.23

---
+++ WebContent/Study/etc/out.js
@@ -0,0 +1,7 @@
+function sizeUp() {
+    document.getElementById('demo').style.fontSize='35px';
+}
+
+function hide() {
+    document.getElementById('demo').style.display='none';
+}
---
+++ src/common/DAO.java
@@ -0,0 +1,34 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DAO {
+	public static Connection getConect() {          // Connection 인터페이스 
+		Connection conn = null;   // null 값으로 초기화
+		
+		// DB 접속정보
+		String user = "hr";       // 접속할 곳 아이디
+		String passwd ="hr";      // 접속할 곳 비밀번호
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";  // 사용하려는 데이터베이스명을 포함한 URL 기술
+		
+		// 드라이버 로딩변수 driver 선언
+		String driver = "oracle.jdbc.driver.OracleDriver";   
+	
+		try {
+			// 1. 드라이버 로딩
+			Class.forName(driver);
+			
+			// 2. 연결
+			conn = DriverManager.getConnection(url, user, passwd);
+		
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
+
